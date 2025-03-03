2f16308c4b0a18727ae663f643fa0833e05a6665
Alexander Ursu <alexanderu@crytek.de>
2025-03-03 17:25:28 +0100

    Solution with overriden self.mysql_caller

--- lib/puppet/provider/mysql_plugin/mariadb.rb
+++ lib/puppet/provider/mysql_plugin/mariadb.rb
@@ -6,5 +6,25 @@ Puppet::Type.type(:mysql_plugin).provide(
 ) do
   desc 'Manages MySQL plugins.'
 
-  commands mysql_raw: 'mariadb'
+  commands mariadb_raw: 'mariadb'
+
+  def self.mysql_caller(text_of_sql, type)
+    if type.eql? 'system'
+      if File.file?("#{Facter.value(:root_home)}/.mylogin.cnf")
+        ENV['MYSQL_TEST_LOGIN_FILE'] = "#{Facter.value(:root_home)}/.mylogin.cnf"
+        mariadb_raw([system_database, '-e', text_of_sql].flatten.compact).scrub
+      else
+        mariadb_raw([defaults_file, system_database, '-e', text_of_sql].flatten.compact).scrub
+      end
+    elsif type.eql? 'regular'
+      if File.file?("#{Facter.value(:root_home)}/.mylogin.cnf")
+        ENV['MYSQL_TEST_LOGIN_FILE'] = "#{Facter.value(:root_home)}/.mylogin.cnf"
+        mariadb_raw(['-NBe', text_of_sql].flatten.compact).scrub
+      else
+        mariadb_raw([defaults_file, '-NBe', text_of_sql].flatten.compact).scrub
+      end
+    else
+      raise Puppet::Error, _("#mysql_caller: Unrecognised type '%{type}'" % { type: type })
+    end
+  end
 end
